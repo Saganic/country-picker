48bc91cbee2950dc4256fd6c68a246b9fca66a61
Alexander Wunschik <mail@wunschik.it>
2018-05-16 22:41:09 +0200

    code cleanup

--- js/bootstrap-select-country.js
+++ js/bootstrap-select-country.js
@@ -1,89 +1,93 @@
 import countries from "i18n-iso-countries";
-import langs_en from "i18n-iso-countries/langs/en.json";
 
+import langs_en from "i18n-iso-countries/langs/en.json";
 countries.registerLocale(langs_en);
-var langCountries = countries.getNames('en');
-var allCountries = Object.keys(langCountries).map((code) => {
+const langCountries = countries.getNames('en');
+
+const allCountries = Object.keys(langCountries).map((code) => {
 	return {
 		name: langCountries[code],
 		code: code
 	};
 });
 
-let initialize = function(opts) {
-  var countryInput = $(this);
-  var countryList = "";
+let countrypicker = function(opts) {
+	var countryInputs = $(this);
+	var countryList = "";
 
-    //set defaults
-    for (var i = 0; i < countryInput.length; i++) {
+	$(this).each(function(index, select) {
+		var $select = $(select);
 
-        //check if flag
-        var flag = countryInput.eq(i).data('flag');
+		var flag = $select.data('flag');
 
-        if (flag) {
-            countryList = "";
+		var options = [];
+		if (flag) {
+				//for each build list with flag
+				$.each(allCountries, function (index, country) {
+					var flagIcon = `css/flags/${country.code.toLowerCase()}.png`;
+					options.push(`<option
+						data-country-code="${country.code}"
+						data-tokens="${country.code} ${country.name}"
+						style='padding-left:25px;background-position: 4px 7px;background-image:url("${flagIcon}");background-repeat:no-repeat;'
+						value="${country.name}">${country.name}</option>`);
+				});
 
-            //for each build list with flag
-            $.each(allCountries, function (index, country) {
-                var flagIcon = "css/flags/" + country.code.toLowerCase() + ".png";
-                countryList += "<option data-country-code='" + country.code + "' data-tokens='" + country.code + " " + country.name + "' style='padding-left:25px; background-position: 4px 7px; background-image:url(" + flagIcon + ");background-repeat:no-repeat;' value='" + country.name + "'>" + country.name + "</option>";
-            });
+				//add flags to button
+				$select.on('loaded.bs.select', function (e) {
+					var button = $(this).closest('.btn-group').children('.btn');
+					button.hide();
+					var selected = $(this).find(':selected');
+					if (selected && selected.length) {
+						var def = selected.data('country-code').toLowerCase();
+						var flagIcon = "css/flags/" + def + ".png";
+						button.css("background-size", '20px');
+						button.css("background-position", '10px 9px');
+						button.css("padding-left", '40px');
+						button.css("background-repeat", 'no-repeat');
+						button.css("background-image", "url('" + flagIcon + "'");
+					}
+					button.show();
+				});
 
-            //add flags to button
-            countryInput.eq(i).on('loaded.bs.select', function (e) {
-                var button = $(this).closest('.btn-group').children('.btn');
-                button.hide();
-                var def = $(this).find(':selected').data('country-code').toLowerCase();
-                var flagIcon = "css/flags/" + def + ".png";
-                button.css("background-size", '20px');
-                button.css("background-position", '10px 9px');
-                button.css("padding-left", '40px');
-                button.css("background-repeat", 'no-repeat');
-                button.css("background-image", "url('" + flagIcon + "'");
-                button.show();
-            });
+				//change flag on select change
+				$select.on('change', function () {
+					var button = $(this).closest('.btn-group').children('.btn');
+					var selected = $(this).find(':selected');
+					if (selected && selected.length) {
+						var def = selected.data('country-code').toLowerCase();
+						var flagIcon = "css/flags/" + def + ".png";
+						button.css("background-size", '20px');
+						button.css("background-position", '10px 9px');
+						button.css("padding-left", '40px');
+						button.css("background-repeat", 'no-repeat');
+						button.css("background-image", "url('" + flagIcon + "'");
+					}
+				});
+		} else {
+			//for each build list without flag
+			$.each(allCountries, function (index, country) {
+				options.push(`<option
+					data-country-code="${country.code}
+					data-tokens="${country.code} ${country.name}"
+					value="${country.name}">${country.name}</option>`);
+			});
+		}
+		$select.html(options.join('\n'));
 
-            //change flag on select change
-            countryInput.eq(i).on('change', function () {
-                var button = $(this).closest('.btn-group').children('.btn');
-                var def = $(this).find(':selected').data('country-code').toLowerCase();
-                var flagIcon = "css/flags/" + def + ".png";
-                button.css("background-size", '20px');
-                button.css("background-position", '10px 9px');
-                button.css("padding-left", '40px');
-                button.css("background-repeat", 'no-repeat');
-                button.css("background-image", "url('" + flagIcon + "'");
-
-            });
-        }else{
-            countryList ="";
-
-            //for each build list without flag
-            $.each(allCountries, function (index, country) {
-                countryList += "<option data-country-code='" + country.code + "' data-tokens='" + country.code + " " + country.name + "' value='" + country.name + "'>" + country.name + "</option>";
-            });
-
-
-        }
-
-         //append country list
-        countryInput.eq(i).html(countryList);
-
-
-        //check if default
-        var def = countryInput.eq(i).data('default');
-        //if there's a default, set it
-        if (def) {
-            countryInput.eq(i).val(def);
-        }
-
-
-    }
-
+		//check if default
+		var def = $select.data('default');
+		//if there's a default, set it
+		if (def) {
+			$select.val(def);
+		}
+	});
 };
 
-$.fn.countrypicker = initialize;
+/* extend jQuery with the countrypicker function */
+$.fn.countrypicker = countrypicker;
 
+/* initialize all countrypicker by default. This is the default jQuery Behavior. */
 $('.countrypicker').countrypicker();
 
-export default initialize;
+/* return the countrypicker function for use as a module. */
+export default countrypicker;
