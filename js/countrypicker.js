cf9a044389fb416ce0f6dadd846c3d539994873c
Alexander Wunschik <mail@wunschik.it>
2018-05-16 11:12:24 +0200

    fixed flags

--- js/countrypicker.js
+++ js/countrypicker.js
@@ -987,13 +987,13 @@ $(function () {
 
         //check if flag
         flag = countryInput.eq(i).data('flag');
-        
+
         if (flag) {
             countryList = "";
-            
+
             //for each build list with flag
             $.each(countries, function (index, country) {
-                var flagIcon = "css/flags/" + country.code + ".png";
+                var flagIcon = "css/flags/" + country.code.toLowerCase() + ".png";
                 countryList += "<option data-country-code='" + country.code + "' data-tokens='" + country.code + " " + country.name + "' style='padding-left:25px; background-position: 4px 7px; background-image:url(" + flagIcon + ");background-repeat:no-repeat;' value='" + country.name + "'>" + country.name + "</option>";
             });
 
@@ -1001,7 +1001,7 @@ $(function () {
             countryInput.eq(i).on('loaded.bs.select', function (e) {
                 var button = $(this).closest('.btn-group').children('.btn');
                 button.hide();
-                var def = $(this).find(':selected').data('country-code');
+                var def = $(this).find(':selected').data('country-code').toLowerCase();
                 var flagIcon = "css/flags/" + def + ".png";
                 button.css("background-size", '20px');
                 button.css("background-position", '10px 9px');
@@ -1014,7 +1014,7 @@ $(function () {
             //change flag on select change
             countryInput.eq(i).on('change', function () {
                 button = $(this).closest('.btn-group').children('.btn');
-                def = $(this).find(':selected').data('country-code');
+                def = $(this).find(':selected').data('country-code').toLowerCase();
                 flagIcon = "css/flags/" + def + ".png";
                 button.css("background-size", '20px');
                 button.css("background-position", '10px 9px');
@@ -1025,15 +1025,15 @@ $(function () {
             });
         }else{
             countryList ="";
-            
+
             //for each build list without flag
             $.each(countries, function (index, country) {
                 countryList += "<option data-country-code='" + country.code + "' data-tokens='" + country.code + " " + country.name + "' value='" + country.name + "'>" + country.name + "</option>";
             });
-            
-            
+
+
         }
-        
+
          //append country list
         countryInput.eq(i).html(countryList);
 
